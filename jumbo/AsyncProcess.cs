ffb3483c33e2a8be0860e5a33d1cd7f229ca2a3b
sbokor <sbokor@BUNNY>
2019-08-28 00:56:35 +0200

    init

---
+++ jumbo/AsyncProcess.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace jumbo
+{
+  public class AsyncProcess
+  {
+    // WinAPI imports
+
+    [DllImport("kernel32.dll")]
+    internal static extern bool GenerateConsoleCtrlEvent(uint dwCtrlEvent, uint dwProcessGroupId);
+
+    [DllImport("kernel32.dll", SetLastError = false)]
+    internal static extern bool AttachConsole(uint dwProcessId);
+
+    [DllImport("kernel32.dll", SetLastError = false, ExactSpelling = true)]
+    internal static extern bool FreeConsole();
+
+    [DllImport("kernel32.dll")]
+    static extern bool SetConsoleCtrlHandler(ConsoleCtrlDelegate HandlerRoutine, bool Add);
+
+    // Delegate type to be used as the Handler Routine for SCCH
+    delegate Boolean ConsoleCtrlDelegate(uint CtrlType);
+
+    [DllImport("kernel32.dll")]
+    static extern uint SetErrorMode(uint uMode);
+
+    [Flags]
+    public enum ErrorModes : uint
+    {
+      SYSTEM_DEFAULT = 0x0,
+      SEM_FAILCRITICALERRORS = 0x0001,
+      SEM_NOALIGNMENTFAULTEXCEPT = 0x0004,
+      SEM_NOGPFAULTERRORBOX = 0x0002,
+      SEM_NOOPENFILEERRORBOX = 0x8000
+    }
+
+    // Ctrl+C signal
+    internal const int CTRL_C_EVENT = 0;
+
+
+    //
+    public struct ProcResult
+    {
+      public int? ExitCode;
+      public string Output;
+      public string Error;
+    }
+
+
+    //
+    protected class ProcStatus
+    {
+      public Process proc = null;
+      public AutoResetEvent exit_event = new AutoResetEvent(false);
+      public bool running = false;
+    }
+    protected static ProcStatus status = new ProcStatus();
+
+
+    // Delegates
+    public delegate void CallbackEventHandler(string arg);
+    public event CallbackEventHandler OutputCallback;
+    public event CallbackEventHandler ErrorCallback;
+
+
+    //public Process ShellProcess {
+    //  get {
+    //    return process;
+    //  }
+    //}
+
+    //public int Id {
+    //  get {
+    //    if (process == null) return 0;
+    //    return process.Id;
+    //  }
+    //}
+
+    public bool IsRunning {
+      get {
+        //if (status.proc == null) return false;
+        //try {Process.GetProcessById(status.proc.Id);}
+        //catch (InvalidOperationException) { return false; }
+        //catch (ArgumentException){return false;}
+        //return true;
+
+        return status.running;
+     }
+   }
+
+
+
+
+
+
+    //
+    public async Task<int> Run(string command, string arguments, int timeout)
+    {      
+      ProcResult result = new ProcResult();
+      int exit_code = 0;
+
+      using ( status.proc = new Process() )
+      {
+        status.proc.StartInfo.FileName = command;
+        status.proc.StartInfo.Arguments = arguments;
+        status.proc.StartInfo.UseShellExecute = false;       
+        status.proc.StartInfo.ErrorDialog = false;
+
+        //status.proc.StartInfo.RedirectStandardInput = true;
+        status.proc.StartInfo.RedirectStandardOutput = true;
+        status.proc.StartInfo.RedirectStandardError = true;
+        status.proc.StartInfo.CreateNoWindow = true;
+
+        status.proc.EnableRaisingEvents = true;
+
+        status.proc.Exited += (s, e) => {
+          status.running = false;
+        };
+
+        status.proc.Disposed += (s, e) => {
+          status.running = false;
+        };
+
+        //StringBuilder out_buff = new StringBuilder();
+        TaskCompletionSource<bool> out_done = new TaskCompletionSource<bool>();
+
+        status.proc.OutputDataReceived += (s, e) => {
+          if (e.Data == null) {
+            out_done.SetResult(true);
+          }
+          else {
+            //out_buff.Append(e.Data);
+            //out_buff.Append("\r\n");
+            OutputCallback(e.Data);
+          }
+        };
+
+        string err = String.Empty;
+        //StringBuilder err_buff = new StringBuilder();
+        TaskCompletionSource<bool> err_done = new TaskCompletionSource<bool>();
+
+        status.proc.ErrorDataReceived += (s, e) => {
+          if (e.Data == null) {
+            err_done.SetResult(true);
+          }
+          else {
+            //err_buff.Append(e.Data);
+            //err_buff.Append("\r\n");
+            ErrorCallback(e.Data);
+            //
+            err = (string)e.Data.Clone();            
+          }
+        };
+
+        lock(this)
+        {
+          using (ChangeErrorMode mode = new ChangeErrorMode(ErrorModes.SEM_FAILCRITICALERRORS | ErrorModes.SEM_NOGPFAULTERRORBOX))
+          {
+            status.exit_event.Reset();
+
+            if (!status.proc.Start()) {
+              status.running = false;
+              result.ExitCode = status.proc.ExitCode;
+              //return result;
+              return -1;  //
+            }
+            else {
+              status.running = true;
+            }
+
+            //if (!status.proc.Start()) {
+            //  status.running = false;
+            //  //status.exit_event.Set();
+            //  return -1;
+            //}
+            //status.running = true;
+          }
+        }
+
+        // Reads the output stream first and then waits because deadlocks are possible
+        status.proc.BeginOutputReadLine();
+        status.proc.BeginErrorReadLine();
+
+        // Creates task to wait for process exit using timeout
+        Task<bool> timeout_done = Task.Run(() => status.proc.WaitForExit(timeout));
+
+        // Create task to wait for process exit and closing all output streams
+        Task<bool[]> process_done = Task.WhenAll(timeout_done, out_done.Task, err_done.Task);
+
+        // Waits process completion and then checks it was not completed by timeout
+        if (await Task.WhenAny(Task.Delay(timeout), process_done) == process_done && timeout_done.Result)
+        {
+          ////status.proc.CancelOutputRead();
+          ////status.proc.CancelErrorRead();
+
+          result.ExitCode = status.proc.ExitCode;
+          ////result.Output = out_buff.ToString();
+          ////result.Error = err_buff.ToString();
+          result.Error = err;
+          exit_code =  status.proc.ExitCode;
+        }
+
+        else {
+          try { status.proc.Kill(); }
+          catch {}
+        }
+        
+
+        // NEW
+        //bool ok = await Task.WhenAny(Task.Delay(timeout), process_done) == process_done && timeout_done.Result;
+        //if (!ok) {
+        //  try { status.proc.Kill(); }
+        //  catch {}
+        //}
+
+      }  // using process
+
+      status.exit_event.Set(); // <--
+      //return result;
+      return exit_code;
+    }
+
+
+
+    //
+    public bool Abort()
+    {
+      bool ok = ThreadPool.QueueUserWorkItem(new WaitCallback(AbortMethod));
+      return ok;
+    }
+
+
+
+    //
+    protected static void AbortMethod(object data) 
+    {
+      if (status.proc == null) {
+        return;
+      }
+      bool ok = false;
+
+      FreeConsole();
+      AttachConsole((uint)status.proc.Id);
+      SetConsoleCtrlHandler(null, true);
+
+      if (!GenerateConsoleCtrlEvent(CTRL_C_EVENT, 0)) {
+        ok = false;
+      }
+
+      status.exit_event.WaitOne(3000);
+
+      // Die wicked-witch
+      if (status.running) {
+        try { status.proc.Kill(); }
+        catch { }
+      }
+
+      FreeConsole();
+      SetConsoleCtrlHandler(null, false);
+    }
+
+
+
+    //
+    protected struct ChangeErrorMode : IDisposable
+    {
+      private uint old_mode;
+
+      public ChangeErrorMode(ErrorModes mode) {
+        old_mode = SetErrorMode((uint)mode);
+      }
+
+      void IDisposable.Dispose() { 
+        SetErrorMode(old_mode); 
+      }
+    }
+
+
+
+  }  // class
+}    // namespace
+
---
+++ jumbo/Form1.cs
@@ -0,0 +1,295 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace jumbo
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+      Application.Idle += Application_Idle;
+
+      richTextBox1.Tag = richTextBox1.Height;
+      richTextBox1.Visible = false;
+      this.Height -= (int)richTextBox1.Tag;
+    }
+
+    //
+    protected AsyncProcess proc = null;
+    protected AutoResetEvent exit_event = new AutoResetEvent(false);
+    protected string status = "";
+    protected int duration = 0;  // us
+
+
+    
+    //
+    private void Application_Idle(object sender, EventArgs e)
+    {
+      if (string.IsNullOrWhiteSpace(textBox1.Text) ||
+          string.IsNullOrWhiteSpace(textBox2.Text) ||
+          (proc != null && proc.IsRunning))
+      {
+        button1.Enabled = false;
+        button1.ForeColor = Color.White;
+        button1.BackColor = Color.FromArgb(235,235,235);
+      }
+      else {
+        button1.Enabled = true;
+        button1.ForeColor = Color.Black;
+        button1.BackColor = Color.LightGray;
+      }
+
+
+      if (proc != null && proc.IsRunning) {
+        button3.Enabled = true;
+        button3.ForeColor = Color.Black;
+        button3.BackColor = Color.LightGray;
+      }
+      else {
+        button3.Enabled = false;
+        button3.ForeColor = Color.White;
+        button3.BackColor = Color.FromArgb(235,235,235);
+      }
+    }
+
+
+
+    private async void button1_Click(object sender, EventArgs e)
+    {
+      status = "";
+      label3.Text = "Ready";
+      ShowProgress("");
+      richTextBox1.Text = "";
+
+      exit_event.Reset();  //
+
+
+      string cmd = @"C:\bin\ffmpeg\bin\ffmpeg.exe";
+      string opt = "";
+      int timeout = -1;  //ms
+
+      opt += $"-hide_banner -stats -progress pipe:1 ";  // -v quiet -stats -loglevel info 
+      opt += $"-i {textBox1.Text} ";
+      //opt += $"-map p:6 ";      
+      opt += $"-c copy -bsf:a aac_adtstoasc ";
+      opt += $"{textBox2.Text} ";
+      //opt += $"-ss 00:00:10 -vframes 2 -vf scale=\"400:-1\" -q:v 1 {textBox2.Text}_%01d.jpg ";
+      //opt += $"-y ";
+
+      proc = new AsyncProcess();
+      proc.OutputCallback += new AsyncProcess.CallbackEventHandler(OnOutput);
+      proc.ErrorCallback += new AsyncProcess.CallbackEventHandler(OnError);
+
+      //Task<AsyncProcess.ProcResult> t = proc.Run(cmd, opt, timeout);
+      //AsyncProcess.ProcResult ret = await t;
+
+      Task<int> t = proc.Run(cmd, opt, timeout);
+      int ret = await t;
+
+      if (exit_event.WaitOne(100)) {
+        Application.Exit();
+      }
+
+      label3.Text = "Done";
+
+      //if (ret != 0) {
+      //}
+
+    }
+
+
+
+    protected void OnOutput(string data)
+    {
+      string val = (string)data.Clone();
+
+      if (val.Contains("out_time_us=")) {
+        ShowProgress(val);
+      }
+
+      //this.Invoke(new Action(() => {
+      //  richTextBox1.Text += val + "\r\n";
+      //  richTextBox1.SelectionStart = richTextBox1.Text.Length;
+      //  richTextBox1.ScrollToCaret();
+      //}));
+    }
+
+
+
+    protected void OnError(string data)
+    {
+      string val = (string)data.Clone();
+
+      if (val.Contains("Duration:")) {
+        this.duration = GetDuration(val);
+      }
+      else if (status != "DONLOADING") {
+        this.Invoke(new Action(() => {
+           label3.Text = "Analyzing ...";
+        }));
+      }
+
+      this.Invoke(new Action(() => {
+        richTextBox1.Text += val + "\r\n";
+        richTextBox1.SelectionStart = richTextBox1.Text.Length;
+        richTextBox1.ScrollToCaret();
+      }));
+    }
+
+
+
+    private void button2_Click(object sender, EventArgs e)
+    {
+      if (saveFileDialog1.ShowDialog() == DialogResult.OK) {
+        textBox2.Text = saveFileDialog1.FileName;
+      }
+    }
+
+
+
+    private void button3_Click(object sender, EventArgs e)
+    {
+      proc.Abort();
+    }
+
+
+
+    protected int GetDuration(string line)
+    {
+      line = line.Trim();
+      Regex rx = new Regex(@"^Duration: ([0-9]{2}):([0-9]{2}):([0-9]{2})\.([0-9]+)");
+      Match match = rx.Match(line);
+
+      if (match.Groups.Count != 5) return 0;
+
+      int.TryParse(match.Groups[1].Value, out int h);
+      int.TryParse(match.Groups[2].Value, out int m);
+      int.TryParse(match.Groups[3].Value, out int s);
+
+      int us = ((h * 3600) + (m * 60) + s) * 1000000;
+
+      return us;
+    }
+
+
+
+    protected void ShowProgress(string line)
+    {
+      line = line.Trim();
+
+      if (line == "") {
+        this.Invoke(new Action(() => {
+          progressBar1.Value = 0;
+          progressBar1.Refresh();
+          progressBar1.CreateGraphics().DrawString("0%", new Font("Arial", (float)8.25, FontStyle.Bold), 
+            Brushes.Black, new PointF(progressBar1.Width / 2 - 10, progressBar1.Height / 2 - 7));
+        }));
+        return;
+      }
+
+      Regex rx = new Regex(@"out_time_us=([0-9]+)$");
+      Match match = rx.Match(line);
+
+      if (match.Groups.Count != 2) return;
+
+      double.TryParse(match.Groups[1].Value, out double us);
+
+      double percent = us/(this.duration/100.00);
+      percent = Math.Round(percent, 1);
+
+      this.Invoke(new Action(() => {
+        status = "DONLOADING";
+        label3.Text = $"Downloading ...";
+        progressBar1.Value = (int)(percent * 10);
+        progressBar1.Refresh();
+        progressBar1.CreateGraphics().DrawString(percent.ToString() + "%", new Font("Arial", (float)8.25, FontStyle.Bold), 
+          Brushes.Black, new PointF(progressBar1.Width / 2 - 10, progressBar1.Height / 2 - 7));
+      }));
+    }
+
+
+
+    private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+    {
+      exit_event.Reset();
+      e.Cancel = true;
+
+      if (proc == null || !proc.IsRunning) {
+        e.Cancel = false;
+        Application.Exit();
+        return;
+      }
+
+      string msg = "Abort the download and exit?";
+      DialogResult res = MessageBox.Show(msg, "Warning", MessageBoxButtons.YesNo);
+      if (res == DialogResult.No) {
+        return;
+      }
+
+      proc.Abort();
+      exit_event.Set();  //
+    }
+
+
+
+    private void checkBox1_CheckedChanged(object sender, EventArgs e)
+    {
+      // Show
+      if (checkBox1.Checked) {
+        if (richTextBox1.Tag == null) return; // error
+        richTextBox1.Visible = true;
+        this.Height += (int)richTextBox1.Tag;
+        richTextBox1.Focus();
+        richTextBox1.ScrollToCaret();
+      }
+
+      // Hide
+      else {
+        if (richTextBox1.Tag == null) richTextBox1.Tag = richTextBox1.Height;
+        richTextBox1.Visible = false;
+        this.Height -= (int)richTextBox1.Tag;
+      }
+    }
+
+
+
+    private void textBox1_DragDrop(object sender, DragEventArgs e)
+    {
+      //textBox1.BackColor = Color.White;
+      string url = (string)e.Data.GetData(DataFormats.Text);
+      textBox1.Text = url;
+    }
+
+    private void textBox1_DragEnter(object sender, DragEventArgs e)
+    {
+      //textBox1.BackColor = Color.LightGray;
+    }
+
+    private void textBox1_DragOver(object sender, DragEventArgs e)
+    {
+      e.Effect = DragDropEffects.None;
+      if (e.Data.GetDataPresent(DataFormats.Text)) {
+        e.Effect = DragDropEffects.Copy;
+      }
+    }
+
+
+
+
+
+  }
+}
